7c807c894b6d0bda27a43d7bc8d42ad873776ac5
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-04-09 11:59:27 -0700

    all concatenations changed to template notation and errors fixed

--- 1-ES6-practice/maxwell-rediker/js/app.js
+++ 1-ES6-practice/maxwell-rediker/js/app.js
@@ -10,13 +10,13 @@ const pics = [document.getElementById('left'),
                 document.getElementById('center'), //eslint-disable-line
                 document.getElementById('right')]; //eslint-disable-line
 const list = document.getElementById('productlist');
-const totalClicks = 0;
+let totalClicks = 0;
 const views = [];
 const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = (`img/${name}.jpg`);
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -55,13 +55,13 @@ function handleClick(event) {
     makeChart();
   }
   if (event.target.id === 'image_container') {
-    return alert('Be sure to click directly on an image!!');
+    return alert(`Be sure to click directly on an image!!`);
   }
   totalClicks += 1;
-  for(const i = 0; i < names.length; i++){
+  for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -72,7 +72,7 @@ function handleClick(event) {
 function showList() {
   for(const i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = (`${allProducts[i].name} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
     list.appendChild(liEl);
   }
 }
@@ -123,10 +123,10 @@ document.getElementById('bus').addEventListener('click', function(){
 });
 
 if(localStorage.busmall){
-  console.log('Local storage data exists');
+  console.log(`Local storage data exists`);
   allProducts = JSON.parse(localStorage.busmall)
 } else {
-  console.log('There is no local storage data; initialize app by creating instances');
+  console.log(`There is no local storage data; initialize app by creating instances`);
   for(let i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
